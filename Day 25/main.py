3b77d7a2774039e21793848f632a0ed5c0f26788
PravinSelva5 <pravin.selvarajah.eng@gmail.com>
2021-07-20 20:54:17 -0400

    get familiar with pandas library

--- Day 25/main.py
+++ Day 25/main.py
@@ -1,4 +1,30 @@
 import pandas
 
+'''
 data = pandas.read_csv("weather_data.csv")
 print(data)
+
+print("---------------------------\n")
+print(data["temp"].max())
+print(data["temp"].mean())
+print("---------------------------\n")
+# Get Data in columns
+print(data["condition"])
+print("\n")
+print(data.condition)
+
+print("---------------------------\n")
+print(data[data.temp == data.temp.max()])
+
+
+
+# Create a dataframe from scratch
+
+data_dict = {
+    "students": ["Amy", "James", "Angela"],
+    "scores": [76, 56, 65]
+}
+
+data_2 =pandas.DataFrame(data_dict)
+data_2.to_csv("new_data_2")
+'''
