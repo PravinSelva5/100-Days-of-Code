8bc0c7998bd2105ca332b4ae64b7b2bd119dec13
PravinSelva5 <pravin.selvarajah.eng@gmail.com>
2021-08-21 21:32:26 -0400

    creating a password manager

--- Day 29/main.py
+++ Day 29/main.py
@@ -1,17 +1,52 @@
-from Tkinter import *
+from tkinter import *
 
 # ---------------------------- PASSWORD GENERATOR ------------------------------- #
 
 # ---------------------------- SAVE PASSWORD ------------------------------- #
+def save():
+    website = website_entry.get()
+    email = email_entry.get()
+    password = password.get()
 
+    with open("data.txt", "a") as data_file:
+        data_file.write(f"{website} | {email} | {password}")
+        website_entry.delete(0, END)
+        password_entry.delete(0, END)
 # ---------------------------- UI SETUP ------------------------------- #
-
 window = Tk()
 window.title("Password Manager")
+window.config(padx=50, pady=50)
 
 canvas = Canvas(height=200, width=200)
-logo_img = PhotoImage(file="/Users/PravinSelvarajah/Documents/GitHub/100-Days-of-Code/Day 29/logo.png")
+logo_img = PhotoImage(file="logo.gif")
 canvas.create_image(100, 100, image=logo_img)
-canvas.pack()
+canvas.grid(row=0, column=1)
+
+#Labels
+website_label = Label(text="Website:")
+website_label.grid(row=1, column=1)
+email_label = Label(text="Email/Username:")
+email_label.grid(row=2, column=1)
+password_label = Label(text="Password:")
+password_label.grid(row=3, column=1)
+
+# Entries, width is a property of the entry class not the grid method
+website_entry = Entry(width=35)
+website_entry.grid(row=1, column=1)
+website_entry.focus()
+email_entry = Entry(width=35)
+email_entry.grid(row=2, column=1)
+email_entry.insert(0, "pravin@gmail.com")
+password_entry = Entry(width=21)
+password_entry.grid(row=3, column=1)
+
+# Buttons
+generate_password_button = Button(text="Generate Password")
+generate_password_button.grid(row=3, column=2)
+add_button = Button(text="Add", width=36, command=save)
+add_button.grid(row=4, column=1)
+ 
+
+
 
 window.mainloop()
